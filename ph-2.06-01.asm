a6aca9193b0dbaedd3333f44cd558ed954427039
serhatcevikel <serhatcevikel@yahoo.com>
2016-01-05 02:07:12 +0200

    new files

---
+++ ph-2.06-01.asm
@@ -0,0 +1,17 @@
+# example in page 72-73 of patterson hennessy
+
+.globl main
+.text
+main:
+    li $s1 10 # load imm value 10 in s1
+    li $s2 8
+    li $s3 9
+    li $s4 9
+    bne $s3 $s4 Else # if value in s3 is not equal to value in s4, go to line labeled Else
+    add $s0 $s1 $s2 # if s3=s4, then add values in s1 and s2 to s0
+    j Exit # unconditionally jump to line labeled Exit
+
+Else: sub $s0 $s1 $s2 # if s3<>s4 then subtract s2 from s1 to s0
+
+Exit: li $v0 10 # Sets $v0 to "10" to select exit syscall
+    syscall # exit
---
+++ ph-2.06-03.asm
@@ -0,0 +1,17 @@
+# example in page 72-73 of patterson hennessy
+
+.globl main
+.text
+main:
+    li $s1 10 # load imm value 10 in s1
+    li $s2 8
+    li $s3 9
+    li $s4 9
+    beq $s3 $s4 Then # if value in s3 is equal to value in s4, go to line labeled Then
+    sub $s0 $s1 $s2 # if s3<>s4, then subtract values in s1 and s2 to s0
+    j Exit # unconditionally jump to line labeled Exit
+
+Then: add $s0 $s1 $s2 # if s3=s4 then add s2 and s1 to s0
+
+Exit: li $v0 10 # Sets $v0 to "10" to select exit syscall
+    syscall # exit
