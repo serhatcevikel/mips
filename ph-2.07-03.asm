01ad85e9ef08138e7c62cdd8462323686428354c
Serhat Cevikel <serhatcevikel@yahoo.com>
2016-08-31 02:57:48 +0300

    delete

--- ph-2.07-03.asm
+++
@@ -1,3 +0,0 @@
-# the segment of the stack containing a procedure's saved registers and local variables is called a procedure frame or activation record
-# $fp is the frame pointer to point to the first word of the frame of a procedure
-
